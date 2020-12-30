fef9735f34e6903127bf88cef61d7448c9489667
Arz <zarsalan19@gmail.com>
2020-12-30 03:58:09 -0500

    finalized styling

--- src/Components/NumPad.js
+++ src/Components/NumPad.js
@@ -6,54 +6,54 @@ function numPad({ onClick }) {
       <button className='btn' id='clear' onClick={onClick}>
         AC
       </button>
-      <button className='btn' id='divide' onClick={onClick}>
+      <button className='btn expression' id='divide' onClick={onClick}>
         /
       </button>
-      <button className='btn' id='multiply' onClick={onClick}>
+      <button className='btn expression' id='multiply' onClick={onClick}>
         X
       </button>
 
-      <button className='btn' id='seven' onClick={onClick}>
+      <button className='btn number' id='seven' onClick={onClick}>
         7
       </button>
-      <button className='btn' id='eight' onClick={onClick}>
+      <button className='btn number' id='eight' onClick={onClick}>
         8
       </button>
-      <button className='btn' id='nine' onClick={onClick}>
+      <button className='btn number' id='nine' onClick={onClick}>
         9
       </button>
-      <button className='btn' id='subtract' onClick={onClick}>
+      <button className='btn expression' id='subtract' onClick={onClick}>
         -
       </button>
-      <button className='btn' id='four' onClick={onClick}>
+      <button className='btn number' id='four' onClick={onClick}>
         4
       </button>
-      <button className='btn' id='five' onClick={onClick}>
+      <button className='btn number' id='five' onClick={onClick}>
         5
       </button>
-      <button className='btn' id='six' onClick={onClick}>
+      <button className='btn number' id='six' onClick={onClick}>
         6
       </button>
-      <button className='btn' id='add' onClick={onClick}>
+      <button className='btn expression' id='add' onClick={onClick}>
         +
       </button>
-      <button className='btn' id='one' onClick={onClick}>
+      <button className='btn number' id='one' onClick={onClick}>
         1
       </button>
-      <button className='btn' id='two' onClick={onClick}>
+      <button className='btn number' id='two' onClick={onClick}>
         2
       </button>
-      <button className='btn' id='three' onClick={onClick}>
+      <button className='btn number' id='three' onClick={onClick}>
         3
       </button>
 
-      <button className='btn' id='equals' onClick={onClick}>
+      <button className='btn expression' id='equals' onClick={onClick}>
         =
       </button>
-      <button className='btn' id='zero' onClick={onClick}>
+      <button className='btn number' id='zero' onClick={onClick}>
         0
       </button>
-      <button className='btn' id='decimal' onClick={onClick}>
+      <button className='btn number' id='decimal' onClick={onClick}>
         .
       </button>
     </div>
